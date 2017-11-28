4637dbb992127aa2bab578444c46ed9cd8e97d61
momojunzi <mark.holthaus@gmail.com>
2017-11-27 20:47:41 -0800

    removed http

--- server.js
+++ server.js
@@ -23,9 +23,7 @@ require("./routes/html-routes.js")(app);
 // app.get('/', function(req, res) {
 //   res.render('pages/portfolio');
 // });
-setInterval(function(){
-  http.get("https://damp-sands-66473.herokuapp.com/");
-}, 300000);
+
 
 db.sequelize.sync({ force: true }).then(function() {
   app.listen(PORT, function() {
